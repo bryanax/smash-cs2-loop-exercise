8639098f5f2ee4d6873b2ff3b8dcb1b76cf42e4d
mtgibson2014 <mtgibson@berkeley.edu>
2018-07-13 01:14:48 -0700

    Updated Skeleton Code

--- script.js
+++ script.js
@@ -20,9 +20,10 @@ function draw() {
 /* Exercise 1: While loop squares!
 
 drawSquares is a function that draws multiple squares on the canvas. It takes in a number 
-that specifies how many squares the function needs to draw. Your task is to use a while loop
-to draw 'num' number of rectangles at random positions and with some random size in the canvas.
-Don't forget to fill() in your shapes with various colors!
+that specifies how many squares the function needs to draw. 
+
+Task: Your task is to use a while loop to simply draw 'num' number of rectangles each at random positions
+      and with some random size in the canvas. Don't forget to fill() in your shapes with various colors!
 
 Hint: Some useful variables have been defined for you already! Use these variables and the random()
       function from p5 to help you generate random positions and sizes for your rectangles!
@@ -37,7 +38,9 @@ function drawSquares(num) {
   var randomRed;
   var randomGreen;
   var randomBlue;
-  // <YOUR CODE HERE>
+  while(){
+
+  }
   
 }
 
